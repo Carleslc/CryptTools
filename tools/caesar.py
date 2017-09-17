44ddf1d90f630418965bb0d4e0cccfd00fc2de05
Carleslc <lazaro.costa.carles@gmail.com>
2017-09-17 20:32:51 +0200

    Caesar verbose minor fix

--- tools/caesar.py
+++ tools/caesar.py
@@ -63,10 +63,10 @@ text = read(args.text)
 
 if args.key is not None:
     if args.verbose:
-        print(f"Original text most frequent characters: {most_frequent_chars(text)}\n")
+        print(f"Original text most frequent character: {most_frequent_char(text)}\n")
     encrypted = caesar(text, args.key)
     print(encrypted)
     if args.verbose:
-        print(f"\nEncrypted text most frequent characters: {most_frequent_chars(encrypted)}")
+        print(f"\nEncrypted text most frequent character: {most_frequent_char(encrypted)}")
 else:
     crack(text)
